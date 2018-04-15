dd0903641a0894e39068b067bd98e9ed4bd79d49
zeucxb <zeucxb@gmail.com>
2018-04-15 14:11:52 -0300

    Remove code repetition at account tests

--- account/account_test.go
+++ account/account_test.go
@@ -9,21 +9,22 @@ import (
 
 func TestProccess(t *testing.T) {
 	g := Goblin(t)
+
+	accounts := make(core.Data)
+	transactions := make(core.Data)
+
+	accounts[123] = []int{10000}
+	accounts[234] = []int{15000}
+	accounts[345] = []int{0}
+	accounts[456] = []int{-30}
+	accounts[567] = []int{-30}
+
+	transactions[123] = []int{5000, -200, -30000, 9000, -100}
+	transactions[234] = []int{-15000, -200, -1}
+	transactions[567] = []int{-1000, -10, 30000}
+
 	g.Describe("Account", func() {
 		g.It("Proccess accounts and transactions", func() {
-			accounts := make(core.Data)
-			transactions := make(core.Data)
-
-			accounts[123] = []int{10000}
-			accounts[234] = []int{15000}
-			accounts[345] = []int{0}
-			accounts[456] = []int{-30}
-			accounts[567] = []int{-30}
-
-			transactions[123] = []int{5000, -200, -30000, 9000, -100}
-			transactions[234] = []int{-15000, -200, -1}
-			transactions[567] = []int{-1000, -10, 30000}
-
 			data := Proccess(accounts, transactions)
 
 			g.Assert(data[123]).Equal(-7300)
@@ -34,19 +35,6 @@ func TestProccess(t *testing.T) {
 		})
 
 		g.It("FormatOutput creating a valid csv string", func() {
-			accounts := make(core.Data)
-			transactions := make(core.Data)
-
-			accounts[123] = []int{10000}
-			accounts[234] = []int{15000}
-			accounts[345] = []int{0}
-			accounts[456] = []int{-30}
-			accounts[567] = []int{-30}
-
-			transactions[123] = []int{5000, -200, -30000, 9000, -100}
-			transactions[234] = []int{-15000, -200, -1}
-			transactions[567] = []int{-1000, -10, 30000}
-
 			data := Proccess(accounts, transactions)
 
 			output := FormatOutput(accounts, data)
